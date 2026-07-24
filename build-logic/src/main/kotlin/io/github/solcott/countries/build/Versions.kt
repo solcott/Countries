a2c5f3b5e17581d2348b360fd16e3fb3bee70064
Scott Olcott <scottolcott@gmail.com>
2026-07-23 23:07:18 -0600

    Fixed issue where Versions.kt was accidentally ignored by git.
    **Git**: Modified `.gitignore` to ensure `build` directories located within `src` folders are not ignored by the global `build/` exclusion pattern.

---
+++ build-logic/src/main/kotlin/io/github/solcott/countries/build/Versions.kt
@@ -0,0 +1,10 @@
+package io.github.solcott.countries.build
+
+object Versions {
+    const val compileSdk = 37
+    const val minSdk = 28
+    const val targetSdk = 37
+    const val SOURCE_COMPATIBILITY = 17
+    const val TARGET_COMPATIBILITY = 17
+    const val JVM_TOOLCHAIN = 17
+}
